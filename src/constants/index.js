618835be1f0ffa1c1e34ceec2fd7b560ac762068
UmerQadri <umerqadri@live.com>
2020-04-05 19:04:27 +0500

    Constants - add constants account, token, category and url

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const TOKEN = "a6d7d4d6ee9e1d67fc60c55048cf55fda37ef924";
+export const BASE_URL = "https://gsmtasks.com/api/tasks/tasks/";
+export const ACCOUNT_ID = "7373e51e-bea0-4142-a2c3-ab364b5f755d";
+export const CATEGORY = "assignment";
